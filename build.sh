dedd2546970d982cea1f072647966b4fbb9b873c
Michael Barlow <michaelbarlow7@gmail.com>
2015-11-04 18:19:12 +1100

    Simplifying compilation commands even further

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@ rm *.o
 rm file-gra
 
 # gra.c
-gcc -pthread -I$(gimptool-2.0 --cflags) -DGTK_DISABLE_DEPRECATED -g -O2 -Wall -Wdeclaration-after-statement -Wmissing-prototypes -Werror=missing-prototypes -Wstrict-prototypes -Wmissing-declarations -Winit-self -Wpointer-arith -Wold-style-definition -Wmissing-format-attribute -Wformat-security -Wlogical-op -Wtype-limits -fno-common -fdiagnostics-show-option -Wreturn-type   -MT gra.o -MD -MP -MF .deps/gra.Tpo -c -o gra.o gra.c
+gcc -pthread -I$(gimptool-2.0 --cflags) -DGTK_DISABLE_DEPRECATED -g -O2 -Wall -Wdeclaration-after-statement -Wmissing-prototypes -Werror=missing-prototypes -Wstrict-prototypes -Wmissing-declarations -Winit-self -Wpointer-arith -Wold-style-definition -Wmissing-format-attribute -Wformat-security -Wlogical-op -Wtype-limits -fno-common -fdiagnostics-show-option -Wreturn-type -MD -MP -MF .deps/gra.Tpo -c gra.c
 
 # gra-read.c
-gcc -pthread -I$(gimptool-2.0 --cflags) -DGTK_DISABLE_DEPRECATED -g -O2 -Wall -Wdeclaration-after-statement -Wmissing-prototypes -Werror=missing-prototypes -Wstrict-prototypes -Wmissing-declarations -Winit-self -Wpointer-arith -Wold-style-definition -Wmissing-format-attribute -Wformat-security -Wlogical-op -Wtype-limits -fno-common -fdiagnostics-show-option -Wreturn-type   -MT gra-read.o -MD -MP -MF .deps/gra.Tpo -c -o gra-read.o gra-read.c
+gcc -pthread -I$(gimptool-2.0 --cflags) -DGTK_DISABLE_DEPRECATED -g -O2 -Wall -Wdeclaration-after-statement -Wmissing-prototypes -Werror=missing-prototypes -Wstrict-prototypes -Wmissing-declarations -Winit-self -Wpointer-arith -Wold-style-definition -Wmissing-format-attribute -Wformat-security -Wlogical-op -Wtype-limits -fno-common -fdiagnostics-show-option -Wreturn-type -MD -MP -MF .deps/gra.Tpo -c gra-read.c
 
 # gra-write.c
-gcc -pthread -I$(gimptool-2.0 --cflags) -DGTK_DISABLE_DEPRECATED -g -O2 -Wall -Wdeclaration-after-statement -Wmissing-prototypes -Werror=missing-prototypes -Wstrict-prototypes -Wmissing-declarations -Winit-self -Wpointer-arith -Wold-style-definition -Wmissing-format-attribute -Wformat-security -Wlogical-op -Wtype-limits -fno-common -fdiagnostics-show-option -Wreturn-type   -MT gra-write.o -MD -MP -MF .deps/gra.Tpo -c -o gra-write.o gra-write.c
+gcc -pthread -I$(gimptool-2.0 --cflags) -DGTK_DISABLE_DEPRECATED -g -O2 -Wall -Wdeclaration-after-statement -Wmissing-prototypes -Werror=missing-prototypes -Wstrict-prototypes -Wmissing-declarations -Winit-self -Wpointer-arith -Wold-style-definition -Wmissing-format-attribute -Wformat-security -Wlogical-op -Wtype-limits -fno-common -fdiagnostics-show-option -Wreturn-type -MD -MP -MF .deps/gra.Tpo -c gra-write.c
 
 # link
 libtool --silent --tag=CC   --mode=link gcc  -g -O2 -Wall -Wdeclaration-after-statement -Wmissing-prototypes -Werror=missing-prototypes -Wstrict-prototypes -Wmissing-declarations -Winit-self -Wpointer-arith -Wold-style-definition -Wmissing-format-attribute -Wformat-security -Wlogical-op -Wtype-limits -fno-common -fdiagnostics-show-option -Wreturn-type -o file-gra gra.o gra-read.o gra-write.o $(gimptool-2.0 --libs)
